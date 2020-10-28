e325b2c2e209491a5ba73a0eae36cb67fd9a0679
Marcelo García <chelo_Ge@hotmail.com>
2020-10-27 22:45:21 -0600

    google colab waymo setup script

---
+++ colab_setup.py
@@ -0,0 +1,18 @@
+import os
+from google.colab import auth
+
+os.system('unzip Object_detection_in_urban_environments.zip')
+os.system('pip install -r Object_detection_in_urban_environments/build/requirements.txt')
+os.system('echo "deb http://packages.cloud.google.com/apt gcsfuse-`lsb_release -c -s` main" | sudo tee /etc/apt/sources.list.d/gcsfuse.list')
+os.system('curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -')
+os.system('sudo apt-get -y -q update')
+os.system('sudo apt-get -y -q install gcsfuse')
+auth.authenticate_user()
+os.system('mkdir -p data/training')
+os.system('mkdir -p data/validation')
+os.system('mkdir -p data/testing')
+os.system('gcsfuse --only-dir training/ waymo_open_dataset_v_1_2_0_individual_files data/training/')
+os.system('gcsfuse --only-dir validation/ waymo_open_dataset_v_1_2_0_individual_files data/validation/')
+os.system('gcsfuse --only-dir testing/ waymo_open_dataset_v_1_2_0_individual_files data/testing/')
+
+
